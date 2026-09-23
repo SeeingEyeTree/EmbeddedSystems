05f62b858652ee4feadc8c6e3fb0af59fb1d3138
SeeingEyeTree <malcolmephraim@gmail.com>
2026-09-23 02:25:30 -0400

    work?

--- Lab2/lines.py
+++ Lab2/lines.py
@@ -14,12 +14,14 @@ def clear_sensehat():
             sense.set_pixel(c, r, 0)
 
 
-for i in range(6):
+for i in range(7):
     x = i
-    for j in range(8):
+    for j in range(7):
         y = j
         slope = (y_end - y) / (x_end - x) if (x_end - x) != 0 else None
         hat.set_pixel(x, y, (int(10* slope), 0, 50))
+        for k in range(7 - x):
+            hat.set_pixel(x + k, int(y + slope), (int(10* slope), 0, 50))
         time.sleep(0.2)
         clear_sensehat()
         
